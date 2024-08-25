ed0338119d96ceab2a5fdcad254f4b94aea34e10
Swaraj Mohaptra <110276137+swarajcodes@users.noreply.github.com>
2024-08-25 14:13:43 +0530

    important commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,4 +70,5 @@ dependencies {
 
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation ("androidx.compose.material:material-icons-extended:1.6.8")
+    implementation("androidx.core:core-splashscreen:1.0.1")
 }
--- app/src/main/java/com/swaraj/algorythm/screens/HomeScreen.kt
+++ app/src/main/java/com/swaraj/algorythm/screens/HomeScreen.kt
@@ -1,5 +1,7 @@
 package com.swaraj.algorythm.screens
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,44 +11,37 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.DateRange
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Star
-import androidx.compose.material3.BottomAppBarDefaults.containerColor
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.swaraj.algorythm.R
-import java.lang.reflect.Modifier
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(navController: NavController) {
-    var selectedItem by remember { mutableIntStateOf(0) }
-    val items = listOf("Sorting", "Searching", "Data Structures")
+    //var selectedItem by remember { mutableIntStateOf(0) }
+    //val items = listOf("Sorting", "Searching", "Data Structures")
     Scaffold(
         topBar = {
             TopAppBar(
@@ -100,48 +95,61 @@ fun HomeScreen(navController: NavController) {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Spacer(modifier = androidx.compose.ui.Modifier.padding(16.dp))
-            SortingTiles(navController = navController, algoName = "Bubble Sort", screenName = "BubbleSort", timeComp = "")
-            SortingTiles(navController = navController, algoName = "Insertion Sort", screenName = "InsertionSort", timeComp = "")
-            SortingTiles(navController = navController, algoName = "Merge Sort", screenName = "MergeSort", timeComp = "")
-            SortingTiles(navController = navController, algoName = "Quick Sort", screenName = "QuickSort", timeComp = "")
-            SortingTiles(navController = navController, algoName = "Selection Sort", screenName = "SelectionSort", timeComp = "")
+            SortingTiles(navController = navController, algoName = "Bubble Sort", screenName = "BubbleSort", timeComp = "", image = "bs")
+            SortingTiles(navController = navController, algoName = "Insertion Sort", screenName = "InsertionSort", timeComp = "", image = "is")
+            SortingTiles(navController = navController, algoName = "Merge Sort", screenName = "MergeSort", timeComp = "", image = "ms")
+            SortingTiles(navController = navController, algoName = "Quick Sort", screenName = "QuickSort", timeComp = "", image = "qs")
+            SortingTiles(navController = navController, algoName = "Selection Sort", screenName = "SelectionSort", timeComp = "", image = "ss")
         }
     }
 }
 
+@SuppressLint("RememberReturnType", "DiscouragedApi")
 @Composable
 fun SortingTiles(
     navController: NavController,
     algoName: String,
     screenName: String,
     timeComp: String,
-    //image: String
+    image: String
 ) {
     val context = LocalContext.current
-    /*val imageResId = remember(image) {
-        context.resources.getIdentifier(image, "drawable", context.packageName)*/
+    val imageResInt = context.resources.getIdentifier(image,"drawable", context.packageName)
 
-    Surface (
-        modifier = androidx.compose.ui.Modifier
-            .fillMaxWidth()
-            .height(160.dp)
-            .clickable { navController.navigate(screenName) }
-            .padding(16.dp),
-        shape = RoundedCornerShape(16.dp),
-        shadowElevation = 8.dp
-    ){
-        Row(
+        Surface(
             modifier = androidx.compose.ui.Modifier
-                .fillMaxSize()
+                .fillMaxWidth()
+                .height(160.dp)
+                .clickable { navController.navigate(screenName) }
                 .padding(16.dp),
-            verticalAlignment = Alignment.CenterVertically
+            shape = RoundedCornerShape(16.dp),
+            shadowElevation = 8.dp
         ) {
-            Text(
-                text = algoName,
-                fontSize = 24.sp,  // Increased font size
-                fontWeight = FontWeight.Bold,
-                lineHeight = 38.sp
-            )
+            Row(
+                modifier = androidx.compose.ui.Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = algoName,
+                    fontSize = 24.sp,  // Increased font size
+                    fontWeight = FontWeight.Bold,
+                    lineHeight = 38.sp
+                )
+                Spacer(modifier = androidx.compose.ui.Modifier.width(20.dp))
+                Image(
+                    painter = painterResource(id = imageResInt),
+                    contentDescription = null,
+                    contentScale = ContentScale.FillBounds,
+                    modifier = androidx.compose.ui.Modifier
+                        .size(130.dp)  // Increased image size
+                        .clip(RoundedCornerShape(12.dp))
+                        .padding(end = 0.dp)
+                        //.align(Alignment.CenterVertically)
+
+                )
+            }
         }
     }
-}
